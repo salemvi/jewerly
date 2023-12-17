54f51f05b915cd7a33979e41cb11823a5428577f
Vadim <zamanovvr@gmail.com>
2023-12-17 14:14:24 +0300

    add npm lib and create header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.wrapper}>
+            <Link href='/'>
+                <h1 className={styles.logo}>
+                    <span>J</span>EWERLY
+                </h1>
+            </Link>
+            <div className={styles.menuWrapper}>
+                {/* тут будет навигация (клинетский компонент) */}
+                <Link href='/' className={styles.navIcon}>
+                    <Image src='/image/search.svg' width={19} height={19} alt='search' />
+                </Link>
+                <Link href='/' className={styles.navIcon}>
+                    <Image src='/image/cart.svg' width={19} height={19} alt='basket of goods' />
+                </Link>
+                <Link href='/' className={styles.navIcon}>
+                    <Image src='/image/user.svg' width={19} height={19} alt='user page' />
+                </Link>
+            </div>
+        </header>
+    );
+}
+export default Header
